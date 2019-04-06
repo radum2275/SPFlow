4a6c38d5e3001421547e3924b37f440f89b1e18e
Hari Teja <contactme.hariteja@uga.edu>
2019-04-05 20:41:23 -0400

    Add mean, covariance, curr_mean and curr_cov parameters to Product Node
    These are utilised by oSLRAU algorithm

--- src/spn/structure/Base.py
+++ src/spn/structure/Base.py
@@ -90,8 +90,13 @@ class Sum(Node):
 
 
 class Product(Node):
-    def __init__(self, children=None):
+    def __init__(self, children=None, mean= None, cov = None):
         Node.__init__(self)
+        self.mean = mean
+        self.cov = cov  
+        self.curr_mean = None
+        self.curr_cov = None
+
         if children is None:
             children = []
         self.children = children
