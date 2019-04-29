b63006da4237605f400643b36331d8890390a2a2
Hari Teja <contactme.hariteja@uga.edu>
2019-04-29 17:46:15 -0400

    Add intialisation and evaluation of top_spn

--- src/spn/algorithms/RSPN.py
+++ src/spn/algorithms/RSPN.py
@@ -1,11 +1,14 @@
 import logging
 import numpy as np
 import copy
-from spn.structure.Base import Product, Sum, get_nodes_by_type, eval_spn_top_down, Out_Latent, assign_ids, rebuild_scopes_bottom_up
+from spn.structure.Base import Product, Sum, get_nodes_by_type, eval_spn_top_down, Out_Latent, assign_ids, \
+    rebuild_scopes_bottom_up
 from spn.structure.leaves.parametric.Parametric import In_Latent
 from spn.algorithms.Inference import log_likelihood
 
 from spn.algorithms.oSLRAU import oSLRAU_eval_spn_top_down
+
+from spn.algorithms.oSLRAU import oSLRAU_update_structure
 
 
 class RSPN():
@@ -18,8 +21,6 @@ class RSPN():
         self.template_spn = None
         self.top_spn = None
 
-
-
     def initialise_template(self, initial_spn):
 
         latent_val_node_list = []
@@ -29,7 +30,7 @@ class RSPN():
 
         in_latent_children = []
         for i in range(self.num_latent_variables):
-            in_latent_i = In_Latent(bin_value=0, scope=self.num_variables+i)
+            in_latent_i = In_Latent(bin_value=0, scope=self.num_variables + i)
             in_latent_children.append(in_latent_i)
 
         interface_node_list = []
@@ -58,11 +59,11 @@ class RSPN():
             # interface_spn_i = copy.deepcopy(spn)
             interface_node_list.append(interface_spn_i)
 
-        self.template_spn = Out_Latent(children = interface_node_list)
+        self.template_spn = Out_Latent(children=interface_node_list)
         assign_ids(self.template_spn)
         rebuild_scopes_bottom_up(self.template_spn)
-        return self.template_spn
 
+        return self.template_spn
 
     def initialise_top_spn(self):
 
@@ -75,22 +76,58 @@ class RSPN():
             in_latent_children.append(in_latent_i)
 
         self.top_spn = Sum(weights=weights, children=in_latent_children)
+        assign_ids(self.top_spn)
+        rebuild_scopes_bottom_up(self.top_spn)
 
         return self.top_spn
 
+    def evaluate_top_spn_bottom_up(self, data, unrolled_network_lls_per_node, len_sequence):
+
+        nodes = get_nodes_by_type(self.top_spn)
+        lls_per_node = unrolled_network_lls_per_node[len_sequence-1].copy()
+        dummy_data = np.zeros((data.shape[0], self.num_latent_variables))
+
+        for k, node in enumerate(nodes):
+            if isinstance(node, In_Latent):
+                node.inference_value = np.exp(lls_per_node[:, 1 + k]).reshape(-1, 1)
+
+        lls_per_node = np.zeros((data.shape[0], len(nodes)))
+        log_likelihood(self.top_spn, dummy_data, lls_matrix=lls_per_node)
+
+        return lls_per_node
+
+    def evaluate_top_spn_top_down(self, data, lls_per_node, oSLRAU_params):
+
+        dummy_data = np.zeros((data.shape[0], self.num_latent_variables))
+        instance_ids = np.arange(dummy_data.shape[0])
+        self.template_spn.out_latent_winner = np.zeros((data.shape[0],), dtype=int)
+
+        all_results, nodes_to_update, in_latent_dict = oSLRAU_eval_spn_top_down(self.top_spn, oSLRAU_params,
+                                                                                parent_result=instance_ids,
+                                                                                data=dummy_data,
+                                                                                lls_per_node=lls_per_node)
+
+        for in_latent_node, instances in in_latent_dict.items():
+            interface_node_index = in_latent_node.scope[0] - self.num_variables
+            self.template_spn.out_latent_winner[instances] = interface_node_index
+
+        return
 
     def evaluate_rspn(self, data, len_sequence, oSLRAU_params):
 
-        unrolled_network_lls_per_node = self.evaluate_rspn_bottom_up(self.template_spn, data, len_sequence)
-        nodes_to_update = self.evaluate_rspn_top_down(self.template_spn, data, len_sequence, unrolled_network_lls_per_node,
-                                                 oSLRAU_params)
-        return nodes_to_update
+        unrolled_network_lls_per_node = self.evaluate_rspn_bottom_up(data, len_sequence)
 
+        top_spn_lls_per_node = self.evaluate_top_spn_bottom_up(data, unrolled_network_lls_per_node, len_sequence)
+        self.evaluate_top_spn_top_down(data, top_spn_lls_per_node, oSLRAU_params)
+
+        nodes_to_update = self.evaluate_rspn_top_down(data, len_sequence, unrolled_network_lls_per_node,
+                                                      oSLRAU_params)
+        return nodes_to_update
 
     def evaluate_rspn_bottom_up(self, data, len_sequence):
 
         nodes = get_nodes_by_type(self.template_spn)
-        #lls_per_node = np.zeros((data.shape[0], len(nodes)))
+        # lls_per_node = np.zeros((data.shape[0], len(nodes)))
 
         in_latent_index = []
         for j in range(len(nodes)):
@@ -103,50 +140,34 @@ class RSPN():
             log_likelihood(self.template_spn, data, lls_matrix=lls_per_node)
             unrolled_network_lls_per_node.append(lls_per_node)
             for k, index in enumerate(in_latent_index):
-                nodes[index].inference_value = np.exp(lls_per_node[:, 1+k]).reshape(-1, 1)
+                nodes[index].inference_value = np.exp(lls_per_node[:, 1 + k]).reshape(-1, 1)
 
         return unrolled_network_lls_per_node
-
 
     def evaluate_rspn_top_down(self, data, len_sequence, unrolled_network_lls_per_node, oSLRAU_params):
 
         l = len_sequence
-        num_variables = data.shape[1]
-        self.template_spn.out_latent_winner = np.zeros((data.shape[0],), dtype=int)
+        # num_variables = data.shape[1]
+        # self.template_spn.out_latent_winner = np.zeros((data.shape[0],), dtype=int)
         for i in range(l):
 
             instance_ids = np.arange(data.shape[0])
             data = data
-            lls_per_node = unrolled_network_lls_per_node[l-i-1]
-            all_results, nodes_to_update, in_latent_dict = oSLRAU_eval_spn_top_down(template_spn, oSLRAU_params, parent_result=instance_ids,
-                                     data=data, lls_per_node=lls_per_node)
+            lls_per_node = unrolled_network_lls_per_node[l - i - 1]
+            all_results, nodes_to_update, in_latent_dict = oSLRAU_eval_spn_top_down(self.template_spn, oSLRAU_params,
+                                                                                    parent_result=instance_ids,
+                                                                                    data=data,
+                                                                                    lls_per_node=lls_per_node)
 
             for in_latent_node, instances in in_latent_dict.items():
-                interface_node_index = in_latent_node.scope[0] - num_variables
+                interface_node_index = in_latent_node.scope[0] - self.num_variables
                 self.template_spn.out_latent_winner[instances] = interface_node_index
 
             return nodes_to_update
 
+    def update_template_spn(self, oSLRAU_params, nodes_to_update):
+        nodes = get_nodes_by_type(self.template_spn)
+        tot_nodes_len = len(nodes)
+        self.template_spn = oSLRAU_update_structure(self.template_spn, oSLRAU_params, tot_nodes_len, nodes_to_update)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return self.template_spn
