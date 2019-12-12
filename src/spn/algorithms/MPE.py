078cbe3910abbf001e830072e31be8b225f0b29f
c0derzer0 <contactme.teja@gmail.com>
2019-12-12 16:21:34 -0500

    Add mpe_max and mpe_interface_switch

--- src/spn/algorithms/MPE.py
+++ src/spn/algorithms/MPE.py
@@ -3,12 +3,15 @@ Created on July 02, 2018
 
 @author: Alejandro Molina
 """
-from spn.algorithms.Inference import log_likelihood, sum_log_likelihood, prod_log_likelihood
+from spn.algorithms.Inference import log_likelihood, sum_log_likelihood, \
+    prod_log_likelihood, max_log_likelihood
 from spn.algorithms.Validity import is_valid
-from spn.structure.Base import Product, Sum, InterfaceSwitch, get_nodes_by_type, eval_spn_top_down
+from spn.structure.Base import Product, Sum, InterfaceSwitch, get_nodes_by_type, \
+    eval_spn_top_down, Max
 import numpy as np
 import logging
 
+from spn.structure.leaves.spmnLeaves.SPMNLeaf import LatentInterface
 
 logger = logging.getLogger(__name__)
 
@@ -32,6 +35,27 @@ def mpe_prod(node, parent_result, data=None, lls_per_node=None, rand_gen=None):
 
 
 def mpe_sum(node, parent_result, data=None, lls_per_node=None, rand_gen=None):
+    if parent_result is None:
+        return None
+
+    parent_result = merge_input_vals(parent_result)
+
+    # logging.debug(f'sum node {node}')
+    w_children_log_probs = np.zeros((len(parent_result), len(node.weights)))
+    for i, c in enumerate(node.children):
+        w_children_log_probs[:, i] = lls_per_node[parent_result, c.id] + np.log(node.weights[i])
+
+    max_child_branches = np.argmax(w_children_log_probs, axis=1)
+
+    children_row_ids = {}
+
+    for i, c in enumerate(node.children):
+        children_row_ids[c] = parent_result[max_child_branches == i]
+
+    return children_row_ids
+
+
+def mpe_interface_sum(node, parent_result, data=None, lls_per_node=None, rand_gen=None):
     if parent_result is None:
         return None
 
@@ -77,19 +101,76 @@ def get_mpe_top_down_leaf(node, input_vals, data=None, mode=0):
     input_vals = merge_input_vals(input_vals)
 
     # we need to find the cells where we need to replace nans with mpes
-    data_nans = np.isnan(data[input_vals, node.scope])
+    if isinstance(node, LatentInterface):
+        data_nans = np.isnan(data[input_vals, node.interface_idx])
+    else:
+        data_nans = np.isnan(data[input_vals, node.scope])
 
     n_mpe = np.sum(data_nans)
 
     if n_mpe == 0:
         return None
 
-    data[input_vals[data_nans], node.scope] = mode
+    if isinstance(node, LatentInterface):
+        data[input_vals[data_nans], node.interface_idx] = mode
+        print(f'data at get_mpe_leaf {data[0:10]}')
+    else:
+        data[input_vals[data_nans], node.scope] = mode
 
 
-_node_top_down_mpe = {Product: mpe_prod, Sum: mpe_sum, InterfaceSwitch: mpe_interface_switch}
+def mpe_max(node, parent_result, data=None, lls_per_node=None, rand_gen=None):
+    if len(parent_result) == 0:
+        return None
+
+    parent_result = merge_input_vals(parent_result)
+
+    # assert data is not None, "data must be passed through to max nodes for proper evaluation."
+    decision_value_given = data[parent_result, node.dec_idx]
+
+    w_children_log_probs = np.zeros((len(parent_result), len(node.dec_values)))
+    for i, c in enumerate(node.children):
+        w_children_log_probs[:, i] = lls_per_node[parent_result, c.id]
+        decision_value_given[decision_value_given == node.dec_values[i]] = i
+
+    # Decisions given are not in the set of decisions the node holds.
+    # Use max value
+    # decision_value_given[decision_value_given >= len(node.dec_values)] = np.nan
+
+    max_value = np.argmax(w_children_log_probs, axis=1)
+    # if data contains a decision value use that otherwise use max
+    max_child_branches = np.select([np.isnan(decision_value_given), True],
+                                   [max_value, decision_value_given]).astype(int)
+    dec_branches = max_child_branches[max_child_branches < len(node.dec_values)]
+    dec_value = np.full((len(max_child_branches)), -1)
+
+    dec_value[dec_branches] = node.dec_values[dec_branches]
+
+    children_row_ids = {}
+
+    for i, c in enumerate(node.children):
+        children_row_ids[c] = parent_result[max_child_branches == i]
+    # print("node and max_child_branches", (node.id, node.feature_name, max_child_branches))
+    # decision values at each node
+
+    decision_values = {}
+    max_nodes = {}
+    decision_values[node.feature_name] = np.column_stack((parent_result, dec_value))
+    if len(dec_value) != 0:
+        node_name = [node.name]*len(dec_value)
+        max_nodes[node.feature_name] = np.column_stack((parent_result, node_name))
+    else:
+        max_nodes[node.feature_name] = np.column_stack((parent_result, []))
+
+    # print("w_children_log_probs", w_children_log_probs)
+    return children_row_ids  # decision_values, max_nodes
+
+
+_node_top_down_mpe = {Product: mpe_prod, Sum: mpe_sum, Max: mpe_max,
+                      InterfaceSwitch: mpe_interface_switch
+                      }
 _node_bottom_up_mpe = {}
-_node_bottom_up_mpe_log = {Sum: sum_log_likelihood, Product: prod_log_likelihood}
+_node_bottom_up_mpe_log = {Sum: sum_log_likelihood, Product: prod_log_likelihood,
+                           Max: max_log_likelihood}
 
 def get_node_funtions():
     return [_node_top_down_mpe, _node_bottom_up_mpe]
