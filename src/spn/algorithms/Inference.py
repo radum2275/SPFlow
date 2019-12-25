4ca526e63ab4d23e4ada8733ef070ef278b12754
c0derzer0 <contactme.teja@gmail.com>
2019-12-25 12:33:00 -0500

    Fix max_likelihood and max_log_likelihood
    If a decision does not exist in the set of decision values a max
    node holds, they were returning the best likelihood value. Instead
    change it to return minimum floating point value.

--- src/spn/algorithms/Inference.py
+++ src/spn/algorithms/Inference.py
@@ -44,8 +44,8 @@ def prod_likelihood(node, children, data=None, dtype=np.float64):
 def max_log_likelihood(node, children, data=None, dtype=np.float64):
     llchildren = np.concatenate(children, axis=1)
     assert llchildren.dtype == dtype
-    if llchildren.shape[1] == 1:  # if only one child, then it is max.
-        return llchildren
+    # if llchildren.shape[1] == 1:  # if only one child, then it is max.
+    #     return llchildren
     assert data is not None, "data must be passed through to max nodes for proper evaluation."
     decision_value_given = data[:, node.dec_idx]
     max_value = np.argmax(llchildren, axis=1)
@@ -61,8 +61,7 @@ def max_log_likelihood(node, children, data=None, dtype=np.float64):
 
     # if decision value given is not in children, assign 0 probability
     missing_dec_branch = np.logical_and(np.logical_not(np.isnan(decision_value_given)),np.isnan(d_given))
-    mll[missing_dec_branch] = np.finfo(mll.dtype).min
-
+    mll[missing_dec_branch] = np.log(EPSILON)
     return mll
 
 def max_likelihood(node, children, data=None, dtype=np.float64):
@@ -70,12 +69,29 @@ def max_likelihood(node, children, data=None, dtype=np.float64):
     assert llchildren.dtype == dtype
     # print("node and llchildren", (node,llchildren))
     assert data is not None, "data must be passed through to max nodes for proper evaluation."
-    decision_value_given = data[:,node.dec_idx]
+    # decision_value_given = data[:,node.dec_idx]
+    # max_value = np.argmax(llchildren, axis=1)
+    # # if data contains a decision value use that otherwise use max
+    # child_idx = np.select([np.isnan(decision_value_given), True],
+    #                       [max_value, decision_value_given]).astype(int)
+    # return llchildren[np.arange(llchildren.shape[0]), child_idx].reshape(-1, 1)
+    decision_value_given = data[:, node.dec_idx]
     max_value = np.argmax(llchildren, axis=1)
+    d_given = np.full(decision_value_given.shape[0], np.nan)
+    mapd = {node.dec_values[i]: i for i in range(len(node.dec_values))}
+    for k, v in mapd.items(): d_given[decision_value_given == k] = v
     # if data contains a decision value use that otherwise use max
-    child_idx = np.select([np.isnan(decision_value_given), True],
-                          [max_value, decision_value_given]).astype(int)
-    return llchildren[np.arange(llchildren.shape[0]), child_idx].reshape(-1, 1)
+
+    child_idx = np.select([np.isnan(d_given), True],
+                          [max_value, d_given]).astype(int)
+
+    mll = llchildren[np.arange(llchildren.shape[0]), child_idx].reshape(-1, 1)
+
+    # if decision value given is not in children, assign 0 probability
+    missing_dec_branch = np.logical_and(
+        np.logical_not(np.isnan(decision_value_given)), np.isnan(d_given))
+    mll[missing_dec_branch] = EPSILON
+    return mll
 
 
 def sum_log_likelihood(node, children, data=None, dtype=np.float64):
